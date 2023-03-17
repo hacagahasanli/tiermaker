418e1aee841bde7333c5c7dda2e42963fca28b96
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-17 17:00:22 +0400

    Name a Template

--- src/pages/Template/index.jsx
+++ src/pages/Template/index.jsx
@@ -17,39 +17,82 @@ const Template = () => {
     return <Wrapper>
         <Header />
         <Container>
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="template_name">Name of Template</label>
-                <input
-                    id="template_name"
-                    name="template_name"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.firstName}
-                />
-                <label htmlFor="template_description">Description of Template:</label>
-                <input
-                    id="template_description"
-                    name="template_description"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.lastName}
-                />
-
-                <label htmlFor="email">Email Address</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-
+            <Form onSubmit={formik.handleSubmit}>
+                <InputWrapper>
+                    <Label htmlFor="template_name">Name of Template</Label>
+                    <Input
+                        id="template_name"
+                        name="template_name"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.firstName}
+                        placeholder="Describe the image set, ex. 'Game of Thrones characters'"
+                    />
+                </InputWrapper>
+                <InputWrapper>
+                    <Label htmlFor="template_description">Description of Template:</Label>
+                    <Input
+                        id="template_description"
+                        name="template_description"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.lastName}
+                    />
+                </InputWrapper>
+                <InputWrapper>
+                    <Label htmlFor="email">Email Address</Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                </InputWrapper>
                 <button type="submit">Submit</button>
-            </form>
+            </Form>
         </Container>
     </Wrapper>
 }
 
-const Container = styled.div``
+const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:1rem;
+`
+
+const Input = styled.input`
+     width: 100%;
+     padding: 1rem;
+     background: #000000;
+     border: 1px solid #474747;
+
+     ::placeholder{
+        font-size: 1.4rem;
+        color: white;
+     }
+
+     :focus,:focus-visible,:focus-within{
+        outline: none;
+     }
+`
+
+const Label = styled.label`
+    font-size: 1.3rem;
+    color: #ffffff;
+`
+
+const Form = styled.form`
+    max-width: 780px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+    gap:2rem;
+`
+
+const Container = styled.div`
+    width: 100%;
+`
 
 export default Template

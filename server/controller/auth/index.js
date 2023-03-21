d225b1b1efd60b3cce131715a84088cc8a4696b4
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-21 23:27:36 +0400

    ...login

--- server/controller/auth/index.js
+++ server/controller/auth/index.js
@@ -2,6 +2,9 @@ class Auth {
     async registration(req, res) {
         const { username, password } = req.body
     }
+    async login(req, res) {
+        const { username, password } = req.body
+    }
 }
 
 export const AuthController = new Auth()
